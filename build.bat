e2026d8e2148bf652e94ccb978371880f9b49c27
av <a@v>
2026-08-07 17:06:05 +0530

    updated build script and added new exe

--- build.bat
+++ build.bat
@@ -11,14 +11,8 @@ if not exist "%outPath%" mkdir "%outPath%"
 
 :: Windows
 if exist "%distPath%\wifi-show-ashv-win_x64.exe" (
-    move /Y "%distPath%\wifi-show-ashv-win_x64.exe" "%outPath%\WiFiShow.exe" >nul
-    echo Created Output\WiFiShow.exe
-)
-
-:: Mac
-if exist "%distPath%\wifi-show-ashv-mac_universal" (
-    move /Y "%distPath%\wifi-show-ashv-mac_universal" "%outPath%\WiFiShow-Mac" >nul
-    echo Created Output\WiFiShow-Mac
+    move /Y "%distPath%\wifi-show-ashv-win_x64.exe" "%outPath%\" >nul
+    echo Moved wifi-show-ashv-win_x64.exe to Output folder.
 )
 
 echo Build complete! Single file executables are in the Output folder.
